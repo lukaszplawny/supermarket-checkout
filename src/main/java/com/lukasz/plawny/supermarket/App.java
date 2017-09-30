fcedc79e070bd865213938c24c01b2967b7dfc9f
Łukasz Pławny <lukasz.plawny@gmail.com>
2017-09-30 20:59:10 +0200

    initial commit

---
+++ src/main/java/com/lukasz/plawny/supermarket/App.java
@@ -0,0 +1,13 @@
+package com.lukasz.plawny.supermarket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
