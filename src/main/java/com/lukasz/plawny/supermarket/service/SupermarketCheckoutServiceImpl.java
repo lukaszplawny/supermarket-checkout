0c2deea89e3b536ce01dfc82f68df7f47f11bbfe
Łukasz Pławny <lukasz.plawny@gmail.com>
2017-09-30 22:23:28 +0200

    checkoutService interface partialy implemented

--- src/main/java/com/lukasz/plawny/supermarket/service/SupermarketCheckoutServiceImpl.java
+++ src/main/java/com/lukasz/plawny/supermarket/service/SupermarketCheckoutServiceImpl.java
@@ -3,18 +3,26 @@ package com.lukasz.plawny.supermarket.service;
 import java.util.List;
 
 import com.lukasz.plawny.supermarket.data.Item;
+import com.lukasz.plawny.supermarket.data.ShoppingCart;
 
 public class SupermarketCheckoutServiceImpl implements SupermarketCheckoutService{
+	
+	private final ShoppingCart shoppingCartForScannedItems;
+	private int actualTotalPrice;
 
+	public SupermarketCheckoutServiceImpl() {
+		shoppingCartForScannedItems = new ShoppingCart();
+		actualTotalPrice = 0;
+	}
 
 	public int checkout(List<Item> items) {
-		// TODO Auto-generated method stub
+		shoppingCartForScannedItems.addItems(items);
+		// TODO calculate actual total price
 		return 0;
 	}
 
 	public int getActualTotalPrice() {
-		// TODO Auto-generated method stub
-		return 0;
+		return actualTotalPrice;
 	}
 
 }
