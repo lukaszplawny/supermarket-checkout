56bfd1b9f093c161cce47b27152a17b84f4e8797
Łukasz Pławny <lukasz.plawny@gmail.com>
2017-10-04 23:04:12 +0200

    cli for checkout service

---
+++ src/main/java/com/lukasz/plawny/supermarket/cli/CheckoutCliLauncher.java
@@ -0,0 +1,10 @@
+package com.lukasz.plawny.supermarket.cli;
+
+public class CheckoutCliLauncher {
+
+	public static void main(String[] argsv) {
+		CheckoutCli cli = new CheckoutCli();
+		cli.runCliCheckout(argsv);
+	}
+
+}
